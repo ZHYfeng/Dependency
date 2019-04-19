2f65dbf98f85a702e434c449c27e817ed24b92cb
Yu Hao <yhao016@ucr.edu>
2019-04-18 17:28:58 -0700

    Update dra.cpp

--- tools/DRA/dra.cpp
+++ tools/DRA/dra.cpp
@@ -12,7 +12,7 @@
 
 llvm::cl::opt<std::string> objdump("objdump", llvm::cl::desc("The path of objdump."), llvm::cl::init("./vmlinux.objdump"));
 llvm::cl::opt<std::string> AssemblySourceCode("asm", llvm::cl::desc("The path of assembly source code."), llvm::cl::init("./build-in.s"));
-
+a
 llvm::cl::opt<std::string> InputFilename(llvm::cl::Positional, llvm::cl::desc("<input bitcode>"), llvm::cl::init("./built-in.bc"));
 
 llvm::cl::opt<std::string> coverfile("coverfile", llvm::cl::desc("The path of cover file."), llvm::cl::init("./cover.txt"));
