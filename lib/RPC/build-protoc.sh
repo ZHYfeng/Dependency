9d2b03dbc4d485ff668858125e530fb553055651
Yu <yhao016@ucr.edu>
2019-05-15 10:22:11 -0700

    feat: add related syscall, maybe work

--- lib/RPC/build-protoc.sh
+++ lib/RPC/build-protoc.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-protoc --cpp_out=. --grpc_out=. --plugin=protoc-gen-grpc=/usr/local/bin/grpc_cpp_plugin ./DependencyRPC.proto ./a2l.proto
+protoc --cpp_out=. --grpc_out=. --plugin=protoc-gen-grpc=/usr/local/bin/grpc_cpp_plugin ./DependencyRPC.proto
+protoc --cpp_out=. ./a2l.proto
