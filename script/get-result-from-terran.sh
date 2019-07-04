7a2e06975a42e7944c6509e521cc2fe89857ee73
yhao <1129719536@qq.com>
2019-07-04 11:26:01 -0700

    frat: add script to get result files from terran

---
+++ script/get-result-from-terran.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+scp yuh@terran.cs.ucr.edu:/data/yhao/benchmark/linux/result/result-cpp.log .
+scp yuh@terran.cs.ucr.edu:/data/yhao/benchmark/linux/result/result-syzkaller.log .
+scp yuh@terran.cs.ucr.edu:/data/yhao/benchmark/linux/result/dependency.log .
+scp yuh@terran.cs.ucr.edu:/data/yhao/benchmark/linux/result/cover_uncover.txt .
+
