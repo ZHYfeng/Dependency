b35695fedd1b1b355fe1c159c7cc293a8f4ff345
Yu Hao <1129719536@qq.com>
2019-07-22 13:43:08 -0700

    test: add condition for syzkaller

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 export LLVM_DIR=/home/yuh/data/build/llvm-7.0.0.src/build
+git pull
 cd ./lib/RPC/
 ./build-protoc.sh
 cd ../../
 echo "[*] Trying to Run Cmake"
 mkdir build
 cd build
-cmake ..
+cmake -DCMAKE_BUILD_TYPE=Debug -G "CodeBlocks - Unix Makefiles" ..
 echo "[*] Trying to make"
 make -j8
