74ea832f847a94f83683d4ef52b1b0fb5091d1cc
Yu Hao <yhao016@ucr.edu>
2019-08-27 15:41:57 -0700

    feat: get uncovered address

--- script/dependency/result/read_result.py
+++ script/dependency/result/read_result.py
@@ -14,6 +14,8 @@ name_with_dra = "result-with-dra"
 name_without_dra = "result-without-dra"
 name_stat = "statistics.bin"
 name_stat_result = "statistics.txt"
+name_data = "data.bin"
+name_data_result = "data.txt"
 length = 1 * 24 * 60
 time_run = length * 60  # second
 
@@ -22,9 +24,8 @@ def stat_print(f, stat):
     f.write(str(stat))
 
 
-def stat_read(dir_path, file_name):
-    # Read the existing Statistics.
-    file_stat = os.path.join(dir_path, file_name)
+def stat_read(dir_path):
+    file_stat = os.path.join(dir_path, name_stat)
     stat = pb.Statistics()
     f = open(file_stat, "rb")
     stat.ParseFromString(f.read())
@@ -38,19 +39,37 @@ def stat_read(dir_path, file_name):
     return stat
 
 
-def read_results(path):
+def data_read(dir_path):
+    file_data = os.path.join(dir_path, name_data)
+    data = pb.Corpus()
+    if os.path.exists(file_data):
+        f = open(file_data, "rb")
+        data.ParseFromString(f.read())
+        f.close()
+
+        data_deal(dir_path, data)
+    return data
+
+
+def data_deal(dir_path, data):
+    file_result = os.path.join(dir_path, name_data_result)
+    f = open(file_result, "w")
+    f.write(str(data))
+    f.close()
+
+
+def stats_read(path):
     stats = []
     if os.path.exists(path):
         for (dir_path, dir_names, file_names) in os.walk(path):
             for file_name in file_names:
                 if file_name.startswith(name_stat):
-                    path_dev = os.path.join(dir_path, file_name)
-                    stats.append(stat_read(path_dev))
+                    stats.append(stat_read(dir_path))
 
     return stats
 
 
-def plot_result(name, x_axis, y_axis):
+def axis_plot(name, x_axis, y_axis):
     if not do_figure or len(y_axis) == 0:
         return
     f = plt.figure()
@@ -62,7 +81,7 @@ def plot_result(name, x_axis, y_axis):
     plt.close(f)
 
 
-def plot_results(name, x_axis, y_axises, labels):
+def axises_plot(name, x_axis, y_axises, labels):
     if not do_figure or len(y_axises) == 0:
         return
     f = plt.figure()
@@ -127,30 +146,32 @@ def stat_deal(stat):
     return r
 
 
-def get_average(stats):
+def stats_get_average(stats):
     r = pb.Statistics()
     for stat in stats:
         for s in stat.stat:
             r.stat[s].name = stat.stat[s].name
             s_add(r.stat[s], stat.stat[s])
-    length = len(stats)
+    s_length = len(stats)
     for s in r.stat:
-        r.stat[s].executeNum = int(r.stat[s].executeNum / length)
-        r.stat[s].time = int(r.stat[s].time / length)
-        r.stat[s].newTestCaseNum = int(r.stat[s].newTestCaseNum / length)
-        r.stat[s].newAddressNum = int(r.stat[s].newAddressNum / length)
+        r.stat[s].executeNum = int(r.stat[s].executeNum / s_length)
+        r.stat[s].time = int(r.stat[s].time / s_length)
+        r.stat[s].newTestCaseNum = int(r.stat[s].newTestCaseNum / s_length)
+        r.stat[s].newAddressNum = int(r.stat[s].newAddressNum / s_length)
     return r
 
 
-def result_deal(dir_path, file_name):
-    stat = stat_read(dir_path, file_name)
+def result_deal(dir_path):
+    stat = stat_read(dir_path)
     x_axis, y_axis = stat_get_time_coverage(stat)
     file_figure = os.path.join(dir_path, "coverage.pdf")
-    plot_result(file_figure, x_axis, y_axis)
-    return stat, x_axis, y_axis
+    axis_plot(file_figure, x_axis, y_axis)
+
+    data = data_read(dir_path)
+    return stat, x_axis, y_axis, data
 
 
-def expansion_axis(length, x_axises, y_axises):
+def axis_expansion(length, x_axises, y_axises):
     for x in x_axises:
         if len(x) != 0:
             max_time = x[-1]
@@ -172,48 +193,66 @@ def expansion_axis(length, x_axises, y_axises):
 
 def result_get_from_sub_dir(stats, x_axises, y_axises, labels, path, name_label):
     if os.path.exists(path):
-        s, x, y = deal_results(path)
+        s, x, y = results_deal(path)
         stats.append(s)
         x_axises.append(x)
         y_axises.append(y)
         labels.append(name_label)
 
 
-def deal_results(path):
+def results_deal(path):
     stats = []
     x_axises = []
     y_axises = []
+    data = []
     labels = []
     if os.path.exists(path):
         for (dir_path, dir_names, file_names) in os.walk(path):
             for file_name in file_names:
                 if file_name.startswith(name_stat):
-                    s, x, y = result_deal(dir_path, file_name)
+                    s, x, y, d = result_deal(dir_path)
                     stats.append(s)
                     x_axises.append(x)
                     y_axises.append(y)
+                    data.append(d)
                     labels.append(dir_path)
 
-    stat = get_average(stats)
+    stat = stats_get_average(stats)
     file_result = os.path.join(path, name_stat_result)
     f = open(file_result, "w")
     stat_print(f, stat)
     stat_print(f, stat_deal(stat))
     f.close()
 
-    x_axises, y_axises = expansion_axis(length, x_axises, y_axises)
+    x_axises, y_axises = axis_expansion(length, x_axises, y_axises)
     x_axis = [sum(e) / len(e) for e in zip(*x_axises)]
     y_axis = [sum(e) / len(e) for e in zip(*y_axises)]
     if len(y_axises) == 0:
         return stats, x_axis, y_axis
     file_figure_average = os.path.join(path, "coverage.pdf")
-    plot_result(file_figure_average, x_axis, y_axis)
+    axis_plot(file_figure_average, x_axis, y_axis)
     file_figure_all = os.path.join(path, "all.pdf")
-    plot_results(file_figure_all, x_axis, y_axises, labels)
+    axises_plot(file_figure_all, x_axis, y_axises, labels)
+
+    uncovered_address = []
+    for d in data:
+        for u in d.uncovered_address:
+            if not u in uncovered_address:
+                uncovered_address.append(u)
+
+    for d in data:
+        for u in uncovered_address:
+            if not u in d.uncovered_address:
+                uncovered_address.remove(u)
+
+    file_result = os.path.join(path, name_data_result)
+    f = open(file_result, "w")
+    f.write(str(uncovered_address))
+    f.close()
     return stats, x_axis, y_axis
 
 
-def deal_dev(dir_path, dir_name):
+def dev_deal(dir_path, dir_name):
     stats = []
     x_axises = []
     y_axises = []
@@ -227,10 +266,10 @@ def deal_dev(dir_path, dir_name):
     path_without_dra = os.path.join(path_dev, name_without_dra)
     result_get_from_sub_dir(stats, x_axises, y_axises, labels, path_without_dra, name_without_dra)
 
-    x_axises, y_axises = expansion_axis(length, x_axises, y_axises)
+    x_axises, y_axises = axis_expansion(length, x_axises, y_axises)
     x_axis = [sum(e) / len(e) for e in zip(*x_axises)]
     file_figure_all = os.path.join(dir_path, dir_name, dir_name + ".pdf")
-    plot_results(file_figure_all, x_axis, y_axises, labels)
+    axises_plot(file_figure_all, x_axis, y_axises, labels)
 
 
 def get_stat_file(path):
@@ -241,18 +280,18 @@ def get_stat_file(path):
     dir_name = os.path.basename(path)
     dir_path = os.path.dirname(path)
     if dir_name.startswith(name_dev):
-        deal_dev(dir_path, dir_name)
+        dev_deal(dir_path, dir_name)
     elif dir_name.startswith(name_with_dra) or dir_name.startswith(name_without_dra):
         path_results = os.path.join(dir_path, dir_name)
-        deal_results(path_results)
+        results_deal(path_results)
     elif dir_name.startswith(name_stat):
-        result_deal(dir_path, dir_name)
+        result_deal(dir_path)
     else:
         for (dir_path, dir_names, file_names) in os.walk(path):
             for dir_name in dir_names:
                 if dir_name.startswith(name_dev):
                     is_dev = True
-                    deal_dev(dir_path, dir_name)
+                    dev_deal(dir_path, dir_name)
             if is_dev:
                 break
 
@@ -260,14 +299,14 @@ def get_stat_file(path):
                 if dir_name.startswith(name_with_dra) or dir_name.startswith(name_without_dra):
                     is_dev = True
                     path_results = os.path.join(dir_path, dir_name)
-                    deal_results(path_results)
+                    results_deal(path_results)
             if is_results:
                 break
 
             for file_name in file_names:
                 if file_name.startswith(name_stat):
                     is_result = True
-                    result_deal(dir_path, file_name)
+                    result_deal(dir_path)
             if is_result:
                 break
 
