a4901714a817d119046326692ff34097a24c2ae7
Yu Hao <yhao016@ucr.edu>
2019-09-02 15:45:44 -0700

    refactor: change name of build.bash

--- build.bash
+++ build.bash
@@ -1,13 +1,13 @@
 #!/bin/bash
 export LLVM_DIR=/home/yuh/data/build/llvm-7.0.0.src/build
 git pull
-cd ./lib/RPC/
+cd ./lib/RPC/ || exit
 ./build-protoc.sh
 cd ../../
 echo "[*] Trying to Run Cmake"
 rm -rf build
 mkdir build
-cd build
+cd build || exit
 cmake -DCMAKE_BUILD_TYPE=Debug -G "CodeBlocks - Unix Makefiles" ..
 echo "[*] Trying to make"
 make -j8
