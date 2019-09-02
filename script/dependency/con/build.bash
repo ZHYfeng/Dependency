693a24e0b44653f291df47d16d0871879b9c67c9
Yu Hao <yhao016@ucr.edu>
2019-09-02 15:52:32 -0700

    feat: auto build dra and syzkaller

--- script/dependency/con/build.bash
+++ script/dependency/con/build.bash
@@ -4,6 +4,8 @@ PATH_GIT=/home/yuh/data/git
 PATH_DRA=$PATH_GIT/2018_dependency
 PATH_SYZKALLER=$PATH_GIT/gopath/src/github.com/google/syzkaller
 cd $PATH_DRA || exit
+git pull
 ./build.bash
 cd $PATH_SYZKALLER || exit
+git pull
 ./build.bash
