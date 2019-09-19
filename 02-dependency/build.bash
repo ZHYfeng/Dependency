8d8e3f0439e621f8b1aa9a396a3415577b436e3b
Yu <yhao016@ucr.edu>
2019-09-19 10:36:42 -0700

    feat: use default CXX

--- 02-dependency/build.bash
+++ 02-dependency/build.bash
@@ -1,6 +1,5 @@
 #!/bin/bash
 export LLVM_DIR=$HOME/data/build/llvm-7.0.0.src/build
-export CXX=$HOME/data/build/llvm-7.0.0.src/build/bin/clang
 echo "[*] Trying to Run Cmake"
 rm -rf build
 mkdir build
