b5a971853087e065d7d026226454d91d82c56c23
Yu <yhao016@ucr.edu>
2019-09-19 15:41:56 -0700

    feat: disable build python

--- build.bash
+++ build.bash
@@ -17,9 +17,9 @@ cd ..
 cd $PATH_SYZKALLER || exit
 bash ./build.bash
 cd ..
-cd $PATH_SCRIPT || exit
-bash ./build.bash
-cd ..
+# cd $PATH_SCRIPT || exit
+# bash ./build.bash
+# cd ..
 
 echo "[*] Trying to generate work dir"
 python3 04-script/main.py generate ~/data/work
