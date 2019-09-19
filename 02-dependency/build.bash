aabcc6b9512d8aa25eb5029de4eb4364b75410c8
Yu <yhao016@ucr.edu>
2019-09-19 10:19:25 -0700

    feat: build release version

--- 02-dependency/build.bash
+++ 02-dependency/build.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
-export LLVM_DIR=$HOME/data/build/llvm-7.0.0.src/build
+export LLVM_DIR=$HOME/data/git/llvm-7.0.0.src/build
 export CXX=$HOME/data/build/llvm-7.0.0.src/build/bin/clang
 echo "[*] Trying to Run Cmake"
 rm -rf build
 mkdir build
 cd build || exit
-cmake -DCMAKE_BUILD_TYPE=Debug -G "CodeBlocks - Unix Makefiles" ..
+cmake ..
 echo "[*] Trying to make"
 make -j8
