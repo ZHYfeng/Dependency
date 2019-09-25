c5d6d0a887dda3c09307a73e9b43c8a4c9dd0fff
Yu <yhao016@ucr.edu>
2019-09-24 20:38:52 -0700

    fix: fix a import bug

--- 05-proto/build-protoc.bash
+++ 05-proto/build-protoc.bash
@@ -5,6 +5,6 @@ protoc --cpp_out=../02-dependency/lib/RPC ./DependencyRPC.proto
 
 protoc --grpc_out=../02-dependency/lib/RPC --plugin=protoc-gen-grpc=/usr/local/bin/grpc_cpp_plugin ./DependencyRPC.proto
 
-protoc -I=. --python_out=../04-script/result ./DependencyRPC.proto
+protoc -I=. --python_out=../04-script/config ./DependencyRPC.proto
 
 protoc --go_out=plugins=grpc:../03-syzkaller/pkg/dra ./DependencyRPC.proto
