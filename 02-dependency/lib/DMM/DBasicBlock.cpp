5494a7ed52a5ce03280141c127745197d86c81f2
yuh <yhao016@ucr.edu>
2019-12-26 11:34:12 -0800

    feat: add new bb count and write statement kind in C++.

--- 02-dependency/lib/DMM/DBasicBlock.cpp
+++ 02-dependency/lib/DMM/DBasicBlock.cpp
@@ -44,20 +44,9 @@ namespace dra {
             i->parent = this;
             i->i = (&it);
 
-            if (llvm::isa<llvm::PHINode>(it) || llvm::isa<llvm::DbgInfoIntrinsic>(it))
-                continue;
-            if (auto *II = llvm::dyn_cast<llvm::IntrinsicInst>(&it)) {
-
-                if (II->getIntrinsicID() == llvm::Intrinsic::lifetime_start ||
-                    II->getIntrinsicID() == llvm::Intrinsic::lifetime_end) {
-                    continue;
-                }
-            }
-
-            this->number_instructions++;
         }
 
-        for (auto temp = b->getNextNode(); !temp->hasName(); temp = temp->getNextNode()) {
+        for (auto temp = b; temp != nullptr && !temp->hasName(); temp = temp->getNextNode()) {
             for (auto &it : *b) {
                 if (llvm::isa<llvm::PHINode>(it) || llvm::isa<llvm::DbgInfoIntrinsic>(it))
                     continue;
