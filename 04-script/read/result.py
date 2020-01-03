ed47cc0e7c0ce0790dbd4e764025c0dddc2b5e49
yuh <yhao016@ucr.edu>
2020-01-02 20:18:21 -0800

    fix: fix a bug

--- 04-script/read/result.py
+++ 04-script/read/result.py
@@ -216,6 +216,9 @@ class Device:
                 ua_status_count[ua_status[ua]] += 1
 
             res = ""
+            res += "default : " + str(ua_status_count[0]) + "\n"
+            res += "not find input : " + str(ua_status_count[1]) + "\n"
+            res += "not find write address : " + str(ua_status_count[2]) + "\n"
             res += "not have write input : " + str(ua_status_count[3]) + "\n"
             res += "cover : " + str(ua_status_count[4]) + "\n"
             res += "unstable write : " + str(ua_status_count[5]) + "\n"
