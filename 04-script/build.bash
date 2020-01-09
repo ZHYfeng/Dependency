383d0e528c3f485008c3737ecd45577e532ea94f
yuh <yhao016@ucr.edu>
2020-01-08 21:03:51 -0800

    feat: script: install python packages

--- 04-script/build.bash
+++ 04-script/build.bash
@@ -1,2 +1,7 @@
 #!/bin/bash
 echo "[*] Trying to build python"
+
+python3 -m pip install --user matplotlib
+python3 -m pip install --user python3-protobuf
+python3 -m pip install --user protobuf
+python3 -m pip install --user scipy
