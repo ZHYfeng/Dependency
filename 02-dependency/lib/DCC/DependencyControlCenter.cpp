7bd5b6f6e93aa27c3efa087dd18abe1d7bccc270
yuh <yhao016@ucr.edu>
2020-02-14 10:02:20 -0800

    feat: get all address of condition

--- 02-dependency/lib/DCC/DependencyControlCenter.cpp
+++ 02-dependency/lib/DCC/DependencyControlCenter.cpp
@@ -629,7 +629,11 @@ namespace dra {
 //        std::ofstream out("address.txt");
 //        std::cout.rdbuf(out.rdbuf());
         for (auto &f : *this->DM.Modules->module) {
+            outputTime("f : " + f.getName().str());
             auto df = this->DM.Modules->get_DF_from_f(&f);
+            if (df == nullptr) {
+                continue;
+            }
             outputTime("df->Path : " + df->Path);
             auto sta = this->getStaticAnalysisResult(df->Path);
             if (sta == nullptr) {
