c5886ad20c6c9e13f244d8ecefedc58693338de4
yuh <yhao016@ucr.edu>
2020-03-08 17:33:07 -0700

    fix: fix a bug in file name

--- 04-script/read_stats.py
+++ 04-script/read_stats.py
@@ -18,22 +18,6 @@ class stats:
                         s = stat(dir_path)
                         self.statistics.append(s)
 
-    def get_average(self):
-        for ss in self.statistics:
-            for s in ss.real_stat.stat:
-                self.processed_stat.real_stat.stat[s].name = ss.real_stat.stat[s].name
-                s_add(self.processed_stat.real_stat.stat[s], ss.real_stat.stat[s])
-        s_length = len(self.statistics)
-        for s in self.processed_stat.real_stat.stat:
-            self.processed_stat.real_stat.stat[s].executeNum = int(
-                self.processed_stat.real_stat.stat[s].executeNum / s_length)
-            self.processed_stat.real_stat.stat[s].time = int(self.processed_stat.real_stat.stat[s].time / s_length)
-            self.processed_stat.real_stat.stat[s].newTestCaseNum = int(
-                self.processed_stat.real_stat.stat[s].newTestCaseNum / s_length)
-            self.processed_stat.real_stat.stat[s].newAddressNum = int(
-                self.processed_stat.real_stat.stat[s].newAddressNum / s_length)
-
-
 class stat:
     def __init__(self, dir_path=""):
         self.dir_path = dir_path
@@ -52,13 +36,6 @@ class stat:
             f = open(file_stat, "rb")
             self.real_stat.ParseFromString(f.read())
             f.close()
-            self.deal_stat()
-
-            # file_result = os.path.join(self.dir_path, devices.name_stat_result)
-            # f = open(file_result, "w")
-            # f.write(str(self.real_stat))
-            # f.write(str(self.deal_stat))
-            # f.close()
 
     def get_time_coverage(self):
         t0 = 0
@@ -72,44 +49,3 @@ class stat:
                 else:
                     num = i.num
                     self.y_axis.append(num)
-
-    def deal_stat(self):
-        s_copy(self.processed_stat.stat[pb.StatGenerate], self.real_stat.stat[pb.StatGenerate])
-        s_copy(self.processed_stat.stat[pb.StatFuzz], self.real_stat.stat[pb.StatFuzz])
-        s_copy(self.processed_stat.stat[pb.StatCandidate], self.real_stat.stat[pb.StatCandidate])
-        s_copy(self.processed_stat.stat[pb.StatTriage], self.real_stat.stat[pb.StatTriage])
-        s_add(self.processed_stat.stat[pb.StatTriage], self.real_stat.stat[pb.StatMinimize])
-        s_copy(self.processed_stat.stat[pb.StatSmash], self.real_stat.stat[pb.StatSmash])
-        s_add(self.processed_stat.stat[pb.StatSmash], self.real_stat.stat[pb.StatHint])
-        s_add(self.processed_stat.stat[pb.StatSmash], self.real_stat.stat[pb.StatSeed])
-        s_copy(self.processed_stat.stat[pb.StatDependency], self.real_stat.stat[pb.StatDependency])
-        s_copy(self.processed_stat.stat[pb.StatDependency], self.real_stat.stat[pb.StatDependencyBoot])
-
-        if self.dir_path != "":
-            f = open(self.file_result, "a")
-            f.write("=====================================================\n")
-            f.write("useful input :" + "\n")
-            for ui in self.real_stat.useful_input:
-                if ui.num > 100:
-                    f.write("program : " + str(ui.input.program) + "\n")
-                    f.write("number : " + str(ui.num) + "\n")
-                    f.write("time : " + str(ui.time) + "\n")
-                    f.write("address : " + str(ui.new_address) + "\n")
-            f.close()
-
-
-def s_add(stat1: pb.Statistic, stat2: pb.Statistic) -> pb.Statistic:
-    stat1.executeNum += stat2.executeNum
-    stat1.time += stat2.time
-    stat1.newTestCaseNum += stat2.newTestCaseNum
-    stat1.newAddressNum += stat2.newAddressNum
-    return stat1
-
-
-def s_copy(stat1: pb.Statistic, stat2: pb.Statistic) -> pb.Statistic:
-    stat1.name = stat2.name
-    stat1.executeNum = stat2.executeNum
-    stat1.time = stat2.time
-    stat1.newTestCaseNum = stat2.newTestCaseNum
-    stat1.newAddressNum = stat2.newAddressNum
-    return stat1
