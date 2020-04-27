0730bc09a7129ea4d96b61c9f2b0dfe14feb3286
Yu <yhao016@ucr.edu>
2020-04-26 20:29:08 -0700

    chore: add python grpc in bash

--- third_party/build.bash
+++ third_party/build.bash
@@ -4,7 +4,11 @@
 # sudo apt install -y autoconf automake libtool curl make g++ unzip build-essential pkg-config libgflags-dev libgtest-dev libc++-dev
 # sudo apt install -y qemu-system-x86
 go get -u github.com/golang/protobuf/protoc-gen-go
-go get -u google.golang.org/grpc python3-protobuf protobuf scipy
+go get -u google.golang.org/grpc 
+
+python3 -m pip install --user python3-protobuf
+python3 -m pip install --user protobuf
+python3 -m pip install --user scipy
 
 PATH_PROTOBUF=protobuf
 PATH_GRPC=grpc
