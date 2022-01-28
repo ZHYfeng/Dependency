d87322f18704c7b2f7d5c5420f449781b718f1d9
Yu Hao <yhao016@ucr.edu>
2022-01-27 22:46:16 -0800

    update run script

--- 04-experiment_script/python/run.py
+++ 04-experiment_script/python/run.py
@@ -7,8 +7,32 @@ import subprocess
 import sys
 import time
 
-import default
 
+time_run = 2 * 24 * 60 * 60  # second
+number_execute = 1
+number_vm_count = 2
+
+path_syzkaller = "/home/icse22ae/Dependency/03-syzkaller"
+path_image = "/home/icse22ae/Dependency/workdir/image"
+path_linux = "/home/icse22ae/Dependency/workdir/13-linux-clang-np"
+path_kernel = os.path.join(path_linux, "arch/x86/boot/bzImage")
+path_vmlinux_objdump = os.path.join(path_linux, "vmlinux.objdump")
+
+name_driver = "built-in"
+name_asm = name_driver + ".s"
+name_bc = name_driver + ".bc"
+name_dra_json = "dra.json"
+name_syzkaller_json = "syzkaller.json"
+file_log_syzkaller = "log_syzkaller.log"
+file_log_dra = "log_dra.log"
+
+path_current = os.getcwd()
+path_dra = "dra"
+path_syzkaller_manager = os.path.join(path_syzkaller, "bin/syz-manager")
+
+name_workdir = "workdir"
+file_image = "stretch.img"
+file_ssh_key = "stretch.id_rsa"
 
 def get_open_port():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -28,60 +52,41 @@ class Process:
         self.path = ""
         self.processes = []
 
-    def execute(self, run_f, dra=True):
-        if dra:
-            name = default.name_with_dra
-        else:
-            name = default.name_without_dra
-
-        path = os.path.join(default.path_current, name, str(self.index))
+    def execute(self, dra=False):
+        path = os.path.join(path_current, str(self.index))
         while os.path.exists(path):
             self.index = self.index + 1
-            path = os.path.join(default.path_current, name, str(self.index))
+            path = os.path.join(path_current, str(self.index))
         self.path = path
         os.makedirs(self.path)
         print(self.path)
 
-        cmd_cp_img = "cp -rf " + default.path_image + " " + os.path.join(self.path, "img")
+        cmd_cp_img = "cp -rf " + path_image + " " + os.path.join(self.path, "img")
         p_cp_img = subprocess.Popen(cmd_cp_img, shell=True, preexec_fn=os.setsid)
         p_cp_img.wait()
 
-        if os.path.exists(default.path_workdir):
-            cmd_cp_corpus = "cp -rf " + default.path_workdir + " " + os.path.join(self.path, default.name_workdir)
-            p_cp_corpus = subprocess.Popen(cmd_cp_corpus, shell=True, preexec_fn=os.setsid)
-            p_cp_corpus.wait()
-
-        cmd_cp_built_in = "cp ./built-in.* " + self.path
-        p_cp_built_in = subprocess.Popen(cmd_cp_built_in, shell=True, preexec_fn=os.setsid)
-        p_cp_built_in.wait()
-
-        cmd_cp_built_in = "cp ./*_serialize " + self.path
-        p_cp_built_in = subprocess.Popen(cmd_cp_built_in, shell=True, preexec_fn=os.setsid)
-        p_cp_built_in.wait()
-
-        cmd_cp_built_in = "cp " + default.name_dra_json + " " + self.path
-        p_cp_built_in = subprocess.Popen(cmd_cp_built_in, shell=True, preexec_fn=os.setsid)
-        p_cp_built_in.wait()
-
-        f = open(os.path.join(default.path_current, default.name_syzkaller_json), "r")
+        f = open(os.path.join(path_current, name_syzkaller_json), "r")
         c = json.load(f)
         f.close()
 
-        c["workdir"] = os.path.join(self.path, default.name_workdir)
-        c["image"] = os.path.join(self.path, "img", default.file_image)
-        c["sshkey"] = os.path.join(self.path, "img", default.file_ssh_key)
+        c["workdir"] = os.path.join(self.path, name_workdir)
+        c["image"] = os.path.join(self.path, "img", file_image)
+        c["sshkey"] = os.path.join(self.path, "img", file_ssh_key)
+        c["syzkaller"] = path_syzkaller
+        c["kernel_obj"] = path_linux
+        c["vm"]["kernel"] = path_kernel
+        c["vm"]["count"] = number_vm_count
 
-        f = open(os.path.join(self.path, default.name_syzkaller_json), "w")
+        f = open(os.path.join(self.path, name_syzkaller_json), "w")
         json.dump(c, f, indent=4)
         f.close()
 
-        run_f.write("cd " + path + "\n")
         os.chdir(self.path)
-        self.execute_syzkaller(run_f, dependency_task=dra)
-        self.execute_dra(run_f)
+        self.execute_syzkaller(dependency_task=dra)
+        self.execute_dra()
 
-    def execute_syzkaller(self, run_f, dependency_task=False, dependency_priority=False):
-        f = open(os.path.join(self.path, default.name_syzkaller_json), "r")
+    def execute_syzkaller(self, dependency_task=False, dependency_priority=False):
+        f = open(os.path.join(self.path, name_syzkaller_json), "r")
         c = json.load(f)
         f.close()
         c["http"] = "127.0.0.1:" + str(get_open_port())
@@ -89,41 +94,28 @@ class Process:
         c["drpc"] = self.drpc
         c["dependency_task"] = dependency_task
         c["dependency_priority"] = dependency_priority
-        f = open(os.path.join(self.path, default.name_syzkaller_json), "w")
+        f = open(os.path.join(self.path, name_syzkaller_json), "w")
         json.dump(c, f, indent=4)
         f.close()
 
-        self.cmd_syzkaller = default.path_syzkaller_manager + " -config=./" + default.name_syzkaller_json + " 2>" + default.file_log_syzkaller + " 1>&2 &"
+        self.cmd_syzkaller = path_syzkaller_manager + " -config=./" + name_syzkaller_json + " 2>" + file_log_syzkaller + " 1>&2 &"
         self.t0 = time.time()
-        self.real_execute(self.cmd_syzkaller, run_f)
+        self.real_execute(self.cmd_syzkaller)
 
-    def execute_dra(self, run_f):
-        self.cmd_dra = default.path_dra + " -asm=" + default.name_asm + " -objdump=" + default.file_vmlinux_objdump \
-                       + " -bc=" + default.name_bc + " -port=" + self.drpc \
-                       + " " + default.name_dra_json + " 1>" + default.file_log_dra + " 2>&1 &"
-        self.real_execute(self.cmd_dra, run_f)
+    def execute_dra(self):
+        self.cmd_dra = path_dra + " -asm=../" + name_asm + " -objdump=" + path_vmlinux_objdump \
+                       + " -bc=../" + name_bc + " -port=" + self.drpc \
+                       + " ../" + name_dra_json + " 1>" + file_log_dra + " 2>&1 &"
+        self.real_execute(self.cmd_dra)
 
-    def real_execute(self, cmd, run_f):
+    def real_execute(self, cmd):
         p = subprocess.Popen(cmd, shell=True)
         # p = subprocess.Popen(cmd, shell=True, start_new_session=True)
         self.processes.append(p)
-        run_f.write(cmd + "\n")
-        run_f.write("PID+=(\"$!\")\n")
-        f = open(os.path.join(self.path, default.file_log_run), "a")
-        f.write("#!/bin/bash\n\n")
-        f.write(cmd + "\n")
-        f.write("#dra pid : " + str(p.pid) + "\n")
-        f.close()
 
     def close(self):
         for p in self.processes:
             os.killpg(os.getpgid(p.pid), signal.SIGTERM)
-
-    def remove(self):
-        os.chdir(self.path)
-        cmd_rm_img = "rm -rf img " + " " + default.name_asm + " " + default.name_bc
-        p_rm_img = subprocess.Popen(cmd_rm_img, shell=True, preexec_fn=os.setsid)
-        p_rm_img.wait()
 
 
 def main():
@@ -131,34 +123,15 @@ def main():
     if len(sys.argv) > 1:
         dra = False
 
-    if dra:
-        path_run = os.path.join(default.path_current, default.name_with_dra, default.name_run_bash)
-    else:
-        path_run = os.path.join(default.path_current, default.name_without_dra, default.name_run_bash)
-    run_f = open(path_run, "a")
-    run_f.write("#!/bin/bash\n\n")
-    run_f.write("PID=()\n")
-
-    tasks = [Process() for i in range(default.number_execute)]
+    tasks = [Process() for i in range(number_execute)]
     for i in tasks:
-        i.execute(run_f, dra)
+        i.execute(dra)
 
-    run_f.write("sleep " + str(default.time_run) + "\n")
-    run_f.write("kill -SIGKILL ${PID[@]}\n")
-    run_f.close()
-
-    cmd_ch = "chmod a+x " + path_run
-    p_ch = subprocess.Popen(cmd_ch, shell=True, preexec_fn=os.setsid)
-    p_ch.wait()
-
-    time.sleep(default.time_run)
+    time.sleep(time_run)
     # time.sleep(30)
 
     for i in tasks:
         i.close()
-
-    for i in tasks:
-        i.remove()
 
 
 if __name__ == "__main__":
